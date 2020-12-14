369e52a132faef4a50a01f78c47fed2db8b4effd
mevikasrajput <vikas9643r@gmail.com>
2020-12-14 23:56:03 +0530

    change UI for gud add js function

--- Our_Review/js/app.js
+++ Our_Review/js/app.js
@@ -1,42 +1,88 @@
 const reviews = [
     {
-        id: 1,
-        name: "Anna",
-        job: "Webdeveloper",
-        img: "https://bit.ly/3m8WscZ",
-        text: "To be fair in certain contexts your professional bio does need to be more formal, like Mr vikas",
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      info:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
     },
 
     {
-        id: 2,
-        name: "Annabee",
-        job: "Webdeveloper",
-        img: "D:\vikas_workspace\javascriptprojects\Our_Review\images",
-        text: "To be fair in certain contexts your professional bio does need to be more formal, like Mr vikas",
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      info:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
     },
 
     {
-        id: 3,
-        name: "Annatwe",
-        job: "Webdeveloper",
-        img: "D:\vikas_workspace\javascriptprojects\Our_Review\images\img-2",
-        text: "To be fair in certain contexts your professional bio does need to be more formal, like Mr vikas",
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      info:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
     },
-];
+
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      info:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
 
 const img = document.getElementById("logo");
-const name = document.getElementById("name");
+const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
 
-const prebtn = document.querySelector(".pre-btn");
-const postbtn = document.querySelector(".post-btn");
-const followbtn = document.querySelector(".follow-btn");
+const prebtn = document.querySelector(".pre-Btn");
+const postbtn = document.querySelector(".post-Btn");
+const followbtn = document.querySelector(".follow-Btn");
 
-let currentItem = 3;
+let currentItem = 2;
 
 window.addEventListener("DOMContentLoaded" , function() {
-    const item = reviews[currentItem];
+    showPerson[currentItem];
+});
+
+
+
+function showPerson(person) {
+    const item = reviews[person];
     img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.info;
+}
+
+prebtn.addEventListener("click" , function() {
+    currentItem++;
+    if(currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+})
+
+postbtn.addEventListener("click" , function() {
+    currentItem--;
+    if(currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+})
+
+followbtn.addEventListener("click" , function() {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
 });
